6b3ed5e8f0925b18bf8fbdbf59fac2aa46db8495
neitsch <nigel.schusters@googlemail.com>
2016-07-17 06:22:49 +0000

    Added missing index.html, moved webpack step to deployment

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@ var gulp = require('gulp');
 var surge = require('gulp-surge');
 var webpack = require('webpack-stream');
 
+gulp.task('copy-index', function() {
+  return gulp.src('index.html')
+    .pipe(gulp.dest('build/'));
+});
+
 gulp.task('scripts', function() {
   return gulp.src('src/entry.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('build/scripts/'));
 });
 
-gulp.task('deploy', [], function () {
+gulp.task('deploy', ['scripts', 'copy-index'], function () {
   return surge({
     project: './build',
     domain: 'curious-class.surge.sh'
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  entry: './src/entry.js',
   output: {
     filename: 'app.bundle.js'
   },
