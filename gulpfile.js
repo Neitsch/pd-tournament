9489d0ff4adb986c24b7242cbcd4d4e40ac31d55
neitsch <nigel.schusters@googlemail.com>
2016-07-17 06:29:50 +0000

    Uglify and https redirect

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var surge = require('gulp-surge');
 var webpack = require('webpack-stream');
+var uglify = require('gulp-uglify');
 
 gulp.task('copy-index', function() {
   return gulp.src('index.html')
@@ -10,12 +11,13 @@ gulp.task('copy-index', function() {
 gulp.task('scripts', function() {
   return gulp.src('src/entry.js')
     .pipe(webpack(require('./webpack.config.js')))
+    .pipe(uglify())
     .pipe(gulp.dest('build/scripts/'));
 });
 
 gulp.task('deploy', ['scripts', 'copy-index'], function () {
   return surge({
     project: './build',
-    domain: 'curious-class.surge.sh'
+    domain: 'https://curious-class.surge.sh'
   })
 })
